dd102615ad58b6fd46d53ded9210e4f947263fff
leirensheng <leirensheng@163.com>
2022-05-16 18:35:24 +0800

    feat: gitlab

---
+++ gitlab.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         gitlab
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  gitlab 自动去掉勾选
+// @author       You
+// @match        http://192.168.1.228:9980/LRS/*/-/merge_requests/new?merge_request*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  const selector = "input[checked=checked]";
+  const target = document.querySelector(selector);
+  console.log(target);
+  if (target) {
+    target.click();
+  }
+
+  // Your code here...
+})();
