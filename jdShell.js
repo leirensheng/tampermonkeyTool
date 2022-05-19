f86d40b3f4e32d30ce0908e43e1dd35a6c81db51
leirensheng <leirensheng@163.com>
2022-05-19 11:35:50 +0800

    feat: 优化

--- jdShell.js
+++ jdShell.js
@@ -4,7 +4,7 @@
 // @version      0.1.1
 // @description  shell 自动登录
 // @author       leirensheng
-// @include      http://leirensheng.dynv6.net:5678
+// @match      http://leirensheng.dynv6.net:5678
 // @icon         https://www.google.com/s2/favicons?domain=notion.so
 // @grant        GM_setClipboard
 // @license MIT
@@ -13,16 +13,9 @@
 (async function () {
   "use strict";
   let $ = (val) => document.querySelector(val);
-  function sleep(time) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, time);
-    });
-  }
   $("[name=username]").value = "admin";
   $("[name=username]").dispatchEvent(new Event("input"));
   $("[name=password]").value = "admin5678";
   $("[name=password]").dispatchEvent(new Event("input"));
   $("[type=submit]").click();
-  await sleep(500);
-  location.href = location.href.replace("/auth", "/home");
 })();
