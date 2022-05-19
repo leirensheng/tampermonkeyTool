da092f6847084adaed4b6af34ea4af09bcf39bf7
leirensheng <leirensheng@163.com>
2022-05-19 11:36:00 +0800

    feat: 修改版本

--- jdShell.js
+++ jdShell.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jdShell
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  shell 自动登录
 // @author       leirensheng
 // @match      http://leirensheng.dynv6.net:5678
