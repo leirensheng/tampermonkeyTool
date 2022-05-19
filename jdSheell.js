e19dbb9588bb18877142943cd63a6420d129dfaf
leirensheng <leirensheng@163.com>
2022-05-19 11:07:48 +0800

    feat: shell自动登录

---
+++ jdSheell.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         jdShell
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  shell 自动登录
+// @author       leirensheng
+// @include      http://leirensheng.dynv6.net:5678/*
+// @icon         https://www.google.com/s2/favicons?domain=notion.so
+// @grant        GM_setClipboard
+// @license MIT
+// ==/UserScript==
+
+(async function () {
+  "use strict";
+  let $ = (val) => document.querySelector(val);
+  function sleep(time) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, time);
+    });
+  }
+  $("[name=username]").value = "admin";
+  $("[name=username]").dispatchEvent(new Event("input"));
+  $("[name=password]").value = "admin5678";
+  $("[name=password]").dispatchEvent(new Event("input"));
+  $("[type=submit]").click();
+  await sleep(500);
+  location.href = location.href.replace("/auth", "/home");
+})();
